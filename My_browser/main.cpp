46d6a0ceaa88b1c3960b7efecafcdca19c992236
wanghuqing <1608751109@qq.com>
2018-12-12 17:57:54 +0800

    2018/12/12

--- My_browser/main.cpp
+++ My_browser/main.cpp
@@ -7,8 +7,8 @@ int main(int argc, char *argv[])
 {
 	QApplication a(argc, argv);
 	a.setWindowIcon(QIcon(":/My_browser/Icon/AppLogoColor.png"));
-	My_browser* w = My_browser::NewInstance();
 	
+	My_browser* w = My_browser::NewInstance();
 	int ret = -1;
 	if (w != NULL)
 	{
